7793ce30e6bfc780ec66b2bc7b65e0f93e511356
sagar hussain <160107305+simplegithubr@users.noreply.github.com>
2024-06-27 15:54:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("******code-with-kinza-object-oriented-program*******");
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const programStart = async (persons) => {
+    do {
+        console.log("welcome!");
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: "whom would you like to intrect with",
+            choices: ["staff", "student", "exit"]
+        });
+        if (ans.select == "staff") {
+            console.log("yoy approch the staff room. please feel free to ask any question.");
+        }
+        else if (ans.select == "student") {
+            const ans = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: "Enter the student's name you wish to engage with"
+            });
+            const student = persons.students.find(val => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`Hello i am ${name.name}. Nice to meet you!`);
+                console.log("New student added");
+                console.log("Current student list:");
+                console.log(persons.students);
+            }
+            else {
+                console.log(`Hello i am ${student.name}. Nice to see you again`);
+                console.log("Existing student list");
+                console.log(persons.students);
+            }
+        }
+        else if (ans.select == "exit") {
+            console.log("Exiting the program...");
+            process.exit();
+        }
+    } while (true);
+};
+programStart(persons);
